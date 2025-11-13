d092047afb2d5e6f4f944fc11619635512e01bdc
PedroBSouza12 <pedrobsouza12@gmail.com>
2025-11-13 16:10:14 +0000

    tentativa de consertar o problema

--- PROJETO/ConectaEsporte/secundarios/urls.py
+++ PROJETO/ConectaEsporte/secundarios/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('profile/', views.perfil_view, name='perfil'),
     path('login/', auth_views.LoginView.as_view(template_name='secundarios/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('home_conta/',views.home_conta_view, name= 'home_conta'),
+    path('post/',views.post_view, name= 'post'),
+    path('feed/',views.feed_view, name= 'feed'),
+
 ]
